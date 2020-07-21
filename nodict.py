acd6d2c832c8310c925faa65cead886cce3b8e11
Ben McKenzie <bmckenzie0522@gmail.com>
2020-07-21 12:03:34 -0400

    passed node tests, working on NoDict

--- nodict.py
+++ nodict.py
@@ -3,38 +3,40 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = '???'
+__author__ = 'Ben Mckenzie, google, keith garcia'
 
 
 class Node:
     def __init__(self, key, value=None):
-        self.hash = None
-        self.key = None
-        self.value = None
-        # Your code here
-        return
+        self.key = key
+        self.value = value
+        self.hash = hash(self.key)
+        
 
     def __repr__(self):
-        # Your code here
-        return
+        return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
-        # Your code here
-        return
+        
+        return self.key == other.key
 
 
 class NoDict:
     def __init__(self, num_buckets=10):
-        self.buckets = None
-        # Your code here
+        self.buckets = [[] for i in range(num_buckets)]
 
     def __repr__(self):
-        # Your code here
-        return
+        """Return a string representing the NoDict contents."""
+        # We want to show all the buckets vertically
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i, bucket in enumerate(self.buckets)])
 
     def add(self, key, value):
-        # Your code here
-        return
+        new_node = Node(key, value)
+        index = new_node.hash % len(self.buckets)
+        for node in self.buckets[index]:
+            if node == new_node:
+                self.buckets[index].remove(node)
+        self.buckets[index].append(new_node)
 
     def get(self, key):
         # Your code here
